2688cedfba3a4f0eeac001bffd34564c959f5e32
Pu Xingyu <pu.stshine@gmail.com>
2024-06-26 07:00:57 +0800

    Add basic models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Useflag(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Package(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=False)
+    git_url = models.TextField(null=False)
+    upstream_url = models.TextField(null=False)
+    license = models.TextField(null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    maintainer = models.ForeignKey(User, on_delete=models.CASCADE)
+    useflags = models.ManyToManyField(Useflag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello GUR!")
