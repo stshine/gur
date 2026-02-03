c9969b274fba3cd9eb99e9b1cf7e7f450feb6c8c
Pu Xingyu <pu.stshine@gmail.com>
2026-02-03 11:15:22 +0800

    backend: Enable authentication system

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
