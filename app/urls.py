69bcb1bda243aca0b27fe95fe972d5b57f493d0d
Pu Xingyu <pu.stshine@gmail.com>
2026-02-05 18:04:49 +0800

    backend: Use custom register function
    Also create a forgejo account when registering

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("accounts/register", views.register, name="register"),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,34 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import os
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+
+from app.services import ForgejoService
+from app.forms import RegisterForm
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def register(request: HttpRequest):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            forgejo_service = ForgejoService(
+                os.environ.get("FORGEJO_APIKEY", ""),
+                os.environ.get("FORGEJO_URL", "http://localhost:3000")
+            )
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password1"]
+            git_user = forgejo_service.create_user(username, email, password)
+            if not git_user:
+                form.add_error(None, "Failed to register user with Forgejo.")
+                return render(request, "registration/register.html", {"form": form})
+
+            form.save()
+            return redirect("/accounts/login")
+    else:
+        form = RegisterForm()
+
+    return render(request, "registration/register.html", {"form": form})
