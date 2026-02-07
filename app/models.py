00057930374a6210e59f8cdda85f4f55a78298b1
Pu Xingyu <pu.stshine@gmail.com>
2026-02-07 16:05:22 +0800

    backend: Display name as representation for Category model

--- app/models.py
+++ app/models.py
@@ -15,6 +15,10 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return self.name
+
+
 class Useflag(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=False)
