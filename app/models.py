aa43eeb80f3d5fc25f6666bad3c342a95e30fb2d
Pu Xingyu <pu.stshine@gmail.com>
2026-02-07 16:04:17 +0800

    backend: Add a model Base

--- app/models.py
+++ app/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class Base(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
